c057aa8d363299a401587fc122dfa118b32b8518
Charles Iliya Krempeaux <iamreiver@gmail.com>
2019-06-12 09:26:53 -0700

    docs

--- doc.go
+++ doc.go
@@ -3,6 +3,10 @@ Package cli provides a way to creating command line interface (CLI) programs, fo
 
 Hello World Handler
 
+WARING: The ‘Hello World Handler’ example you are about to see is meant to give you a sense of how the cli.Handler works.
+If does not use cli.ServerMux. It would be pointless to use pakage cli without using cli.ServerMux.
+Later on we show you a more realistic example using cli.ServerMux. But anyway....
+
 Similar to http.Handler, that is part of the built-in Go "net/http" library, package cli has a cli.Handler.
 
 The definition of cli.Handler is:
