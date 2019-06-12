62d5d458ff1a82e5050c5813c03ed40c0e1540c2
Charles Iliya Krempeaux <iamreiver@gmail.com>
2019-06-12 09:23:16 -0700

    docs

--- doc.go
+++ doc.go
@@ -1,4 +1,46 @@
 /*
 Package cli provides a way to creating command line interface (CLI) programs, for the Go programming language, in a style similar to the "net/http" library that is part of the built-in Go standard library, including support for "middleware".
+
+Hello World Handler
+
+Similar to http.Handler, that is part of the built-in Go "net/http" library, package cli has a cli.Handler.
+
+The definition of cli.Handler is:
+
+	type cli.Handler interface {
+		Run(stdin io.ReadCloser, stdout io.WriteCloser, stderr io.WriteCloser, command ...string) cli.ExitCode
+	}
+
+So, a ‘hello world’ cli.Handler might be:
+
+	import "github.com/reiver/go-cli"
+	
+	// ...
+	
+	type HelloWorldCLIHandler struc{}
+	
+	func (HelloWorldCLIHandler) Run(stdin io.ReadCloser, stdout io.WriteCloser, stderr io.WriteCloser, command ...string) cli.ExitCode {
+		fmt.Fprint(stdout, "Hello world!")
+
+		return cli.ExitCodeOK
+	}
+
+And from your main() function, you might use do something such as:
+
+	import "github.com/reiver/go-cli"
+	
+	// ...
+	
+	func main() {
+		var handler cli.Handler = new(HelloWorldCLIHandler)
+
+		exitcode := handler.Run(os.Stdin, os.Stdout, os.Stderr, os.Args...)
+		os.Exit(exitcode.Code())
+	}
+
+Of course, if this is all you did, you would never use package cli.
+
+The power of package cli is when you use cli.ServerMux.
+
 */
 package cli
