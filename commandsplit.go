f90972b4f230909286913d0cf387cc830a33934b
Charles Iliya Krempeaux <iamreiver@gmail.com>
2019-06-24 16:51:10 -0700

    made commandsplit() use newly publicly exported cliflag.IsFlag()

--- commandsplit.go
+++ commandsplit.go
@@ -1,12 +1,16 @@
 package cli
 
+import (
+	"github.com/reiver/go-cli/flag"
+)
+
 func commandSplit(command ...string) (name []string, parameters []string) {
 	if 1 > len(command) {
 		return []string(nil), []string(nil)
 	}
 
 	for i, token := range command {
-		if 1 < len(token) && '-' == token[0] {
+		if 1 < len(token) && cliflag.IsFlag(token) {
 			parameters = command[i:]
 			break
 		}
